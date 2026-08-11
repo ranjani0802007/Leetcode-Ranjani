6afe4d887781ac80a7cfa559cfc1fc5dc9724660
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-11 14:19:27 +0530

    Create 886-ScoreOfParentheses

---
+++ 886-ScoreOfParentheses/886-ScoreOfParentheses.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 2:19:24 PM
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int score = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(score);
+                score = 0;
+            } else {
+                score = st.pop() + Math.max(2 * score, 1);
+            }
+        }
+
+        return score;
+    }
+}

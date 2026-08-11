14cd4844de15e78d3ea0684a8779a01c1f99eb57
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-11 14:17:10 +0530

    Create 1462-ListTheProductsOrderedInAPeriod

---
+++ 1462-ListTheProductsOrderedInAPeriod/1462-ListTheProductsOrderedInAPeriod.sql
@@ -0,0 +1,12 @@
+-- Last updated: 8/11/2026, 2:17:08 PM
+# Write your MySQL query statement below
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE YEAR(o.order_date) = 2020
+  AND MONTH(o.order_date) = 2
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100;

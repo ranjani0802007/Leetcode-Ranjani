8ad49c449ddf4e532dd50744367e723f023b5dde
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-14 12:34:29 +0530

    File updated at 8/14/2026, 12:34:26 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,31 +1,46 @@
-// Last updated: 8/14/2026, 12:32:36 PM
-1class MinStack {
-2
-3    Stack<Integer> stack = new Stack<>();
-4    Stack<Integer> minStack = new Stack<>();
-5
-6    public MinStack() {
-7    }
+// Last updated: 8/14/2026, 12:34:26 PM
+1class Solution {
+2    public String minWindow(String s, String t) {
+3
+4        if (s.length() < t.length())
+5            return "";
+6
+7        int[] count = new int[128];
 8
-9    public void push(int val) {
-10        stack.push(val);
+9        for (char c : t.toCharArray())
+10            count[c]++;
 11
-12        if (minStack.isEmpty() || val <= minStack.peek())
-13            minStack.push(val);
-14    }
-15
-16    public void pop() {
-17        if (stack.peek().equals(minStack.peek()))
-18            minStack.pop();
+12        int left = 0;
+13        int required = t.length();
+14
+15        int start = 0;
+16        int minLength = Integer.MAX_VALUE;
+17
+18        for (int right = 0; right < s.length(); right++) {
 19
-20        stack.pop();
-21    }
+20            if (count[s.charAt(right)] > 0)
+21                required--;
 22
-23    public int top() {
-24        return stack.peek();
-25    }
+23            count[s.charAt(right)]--;
+24
+25            while (required == 0) {
 26
-27    public int getMin() {
-28        return minStack.peek();
-29    }
-30}
+27                if (right - left + 1 < minLength) {
+28                    minLength = right - left + 1;
+29                    start = left;
+30                }
+31
+32                count[s.charAt(left)]++;
+33
+34                if (count[s.charAt(left)] > 0)
+35                    required++;
+36
+37                left++;
+38            }
+39        }
+40
+41        return minLength == Integer.MAX_VALUE
+42                ? ""
+43                : s.substring(start, start + minLength);
+44    }
+45}

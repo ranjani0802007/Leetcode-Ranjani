74af145d97cdc63508b7390ca51d2095efd40984
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-14 12:32:39 +0530

    File updated at 8/14/2026, 12:32:36 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,64 +1,31 @@
-// Last updated: 8/14/2026, 12:12:23 PM
-1class LRUCache {
+// Last updated: 8/14/2026, 12:32:36 PM
+1class MinStack {
 2
-3    class Node {
-4        int key, value;
-5        Node prev, next;
-6
-7        Node(int key, int value) {
-8            this.key = key;
-9            this.value = value;
-10        }
-11    }
-12
-13    HashMap<Integer, Node> map = new HashMap<>();
-14    Node head = new Node(0, 0);
-15    Node tail = new Node(0, 0);
-16    int capacity;
-17
-18    public LRUCache(int capacity) {
-19        this.capacity = capacity;
-20
-21        head.next = tail;
-22        tail.prev = head;
-23    }
-24
-25    public int get(int key) {
-26        if (!map.containsKey(key))
-27            return -1;
-28
-29        Node node = map.get(key);
-30        remove(node);
-31        insert(node);
-32
-33        return node.value;
-34    }
-35
-36    public void put(int key, int value) {
-37        if (map.containsKey(key))
-38            remove(map.get(key));
-39
-40        Node node = new Node(key, value);
-41        map.put(key, node);
-42        insert(node);
-43
-44        if (map.size() > capacity) {
-45            Node lru = head.next;
-46            remove(lru);
-47            map.remove(lru.key);
-48        }
-49    }
-50
-51    void remove(Node node) {
-52        node.prev.next = node.next;
-53        node.next.prev = node.prev;
-54    }
-55
-56    void insert(Node node) {
-57        node.prev = tail.prev;
-58        node.next = tail;
-59
-60        tail.prev.next = node;
-61        tail.prev = node;
-62    }
-63}
+3    Stack<Integer> stack = new Stack<>();
+4    Stack<Integer> minStack = new Stack<>();
+5
+6    public MinStack() {
+7    }
+8
+9    public void push(int val) {
+10        stack.push(val);
+11
+12        if (minStack.isEmpty() || val <= minStack.peek())
+13            minStack.push(val);
+14    }
+15
+16    public void pop() {
+17        if (stack.peek().equals(minStack.peek()))
+18            minStack.pop();
+19
+20        stack.pop();
+21    }
+22
+23    public int top() {
+24        return stack.peek();
+25    }
+26
+27    public int getMin() {
+28        return minStack.peek();
+29    }
+30}

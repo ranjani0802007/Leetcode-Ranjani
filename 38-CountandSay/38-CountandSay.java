75b3c67cb651295483d110cfa9287a2c631a3d54
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 10:03:46 +0530

    File updated at 8/22/2026, 10:03:46 AM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,28 +1,29 @@
-// Last updated: 8/22/2026, 10:02:36 AM
-1import java.util.*;
-2
-3class Solution {
-4    public int[] topKFrequent(int[] nums, int k) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6
-7        for (int n : nums)
-8            map.put(n, map.getOrDefault(n, 0) + 1);
-9
-10        PriorityQueue<Integer> pq =
-11            new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
-12
-13        for (int n : map.keySet()) {
-14            pq.offer(n);
-15
-16            if (pq.size() > k)
-17                pq.poll();
-18        }
+// Last updated: 8/22/2026, 10:03:46 AM
+1class Solution {
+2    public boolean isValidSudoku(char[][] board) {
+3
+4        boolean[][] row = new boolean[9][9];
+5        boolean[][] col = new boolean[9][9];
+6        boolean[][] box = new boolean[9][9];
+7
+8        for (int i = 0; i < 9; i++) {
+9            for (int j = 0; j < 9; j++) {
+10
+11                if (board[i][j] == '.')
+12                    continue;
+13
+14                int num = board[i][j] - '1';
+15                int b = (i / 3) * 3 + j / 3;
+16
+17                if (row[i][num] || col[j][num] || box[b][num])
+18                    return false;
 19
-20        int[] ans = new int[k];
-21
-22        for (int i = 0; i < k; i++)
-23            ans[i] = pq.poll();
-24
-25        return ans;
-26    }
-27}
+20                row[i][num] = true;
+21                col[j][num] = true;
+22                box[b][num] = true;
+23            }
+24        }
+25
+26        return true;
+27    }
+28}

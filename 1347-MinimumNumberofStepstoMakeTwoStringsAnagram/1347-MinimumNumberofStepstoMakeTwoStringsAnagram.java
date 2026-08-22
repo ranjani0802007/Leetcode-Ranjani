c6079a1c0da61d1f0ed2d19b9652532d171cc5a5
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 11:30:12 +0530

    File updated at 8/22/2026, 11:30:12 AM

--- 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/1347-MinimumNumberofStepstoMakeTwoStringsAnagram.java
+++ 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/1347-MinimumNumberofStepstoMakeTwoStringsAnagram.java
@@ -1,22 +1,25 @@
-// Last updated: 8/22/2026, 11:28:52 AM
+// Last updated: 8/22/2026, 11:30:12 AM
 1class Solution {
-2    public int minSteps(String s, String t) {
+2    public int findMin(int[] nums) {
 3
-4        int[] freq = new int[26];
-5
-6        for (char c : s.toCharArray())
-7            freq[c - 'a']++;
+4        int left = 0;
+5        int right = nums.length - 1;
+6
+7        while (left < right) {
 8
-9        for (char c : t.toCharArray())
-10            freq[c - 'a']--;
-11
-12        int ans = 0;
-13
-14        for (int x : freq) {
-15            if (x > 0)
-16                ans += x;
-17        }
-18
-19        return ans;
-20    }
-21}
+9            int mid = left + (right - left) / 2;
+10
+11            if (nums[mid] < nums[right]) {
+12                right = mid;
+13            }
+14            else if (nums[mid] > nums[right]) {
+15                left = mid + 1;
+16            }
+17            else {
+18                right--;
+19            }
+20        }
+21
+22        return nums[left];
+23    }
+24}

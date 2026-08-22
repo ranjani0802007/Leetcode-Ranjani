e8757f15800190cf440a13bd9ee827bd6356d211
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 10:01:25 +0530

    File updated at 8/22/2026, 10:01:25 AM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,33 +1,27 @@
-// Last updated: 8/22/2026, 9:54:23 AM
-1class Solution {
-2    public String convert(String s, int numRows) {
-3        if (numRows == 1 || numRows >= s.length())
-4            return s;
-5
-6        StringBuilder[] rows = new StringBuilder[numRows];
-7
-8        for (int i = 0; i < numRows; i++)
-9            rows[i] = new StringBuilder();
-10
-11        int row = 0;
-12        int direction = 1;
-13
-14        for (char c : s.toCharArray()) {
-15            rows[row].append(c);
-16
-17            if (row == 0)
-18                direction = 1;
-19            else if (row == numRows - 1)
-20                direction = -1;
-21
-22            row += direction;
-23        }
-24
-25        StringBuilder ans = new StringBuilder();
-26
-27        for (StringBuilder r : rows)
-28            ans.append(r);
-29
-30        return ans.toString();
-31    }
-32}
+// Last updated: 8/22/2026, 10:01:25 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int fourSumCount(int[] nums1, int[] nums2,
+5                            int[] nums3, int[] nums4) {
+6
+7        HashMap<Integer, Integer> map = new HashMap<>();
+8
+9        for (int a : nums1) {
+10            for (int b : nums2) {
+11                int sum = a + b;
+12                map.put(sum, map.getOrDefault(sum, 0) + 1);
+13            }
+14        }
+15
+16        int ans = 0;
+17
+18        for (int c : nums3) {
+19            for (int d : nums4) {
+20                ans += map.getOrDefault(-(c + d), 0);
+21            }
+22        }
+23
+24        return ans;
+25    }
+26}

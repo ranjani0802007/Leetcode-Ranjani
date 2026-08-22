0d0b4439dee23a1d7593183497fa1209436ce4d7
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 10:16:52 +0530

    File updated at 8/22/2026, 10:16:52 AM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,29 +1,41 @@
-// Last updated: 8/22/2026, 10:03:46 AM
-1class Solution {
-2    public boolean isValidSudoku(char[][] board) {
-3
-4        boolean[][] row = new boolean[9][9];
-5        boolean[][] col = new boolean[9][9];
-6        boolean[][] box = new boolean[9][9];
-7
-8        for (int i = 0; i < 9; i++) {
-9            for (int j = 0; j < 9; j++) {
+// Last updated: 8/22/2026, 10:16:52 AM
+1import java.util.*;
+2
+3class RandomizedSet {
+4
+5    ArrayList<Integer> list = new ArrayList<>();
+6    HashMap<Integer, Integer> map = new HashMap<>();
+7    Random random = new Random();
+8
+9    public RandomizedSet() {}
 10
-11                if (board[i][j] == '.')
-12                    continue;
-13
-14                int num = board[i][j] - '1';
-15                int b = (i / 3) * 3 + j / 3;
-16
-17                if (row[i][num] || col[j][num] || box[b][num])
-18                    return false;
-19
-20                row[i][num] = true;
-21                col[j][num] = true;
-22                box[b][num] = true;
-23            }
-24        }
-25
-26        return true;
-27    }
-28}
+11    public boolean insert(int val) {
+12        if (map.containsKey(val))
+13            return false;
+14
+15        map.put(val, list.size());
+16        list.add(val);
+17
+18        return true;
+19    }
+20
+21    public boolean remove(int val) {
+22        if (!map.containsKey(val))
+23            return false;
+24
+25        int index = map.get(val);
+26        int last = list.get(list.size() - 1);
+27
+28        list.set(index, last);
+29        map.put(last, index);
+30
+31        list.remove(list.size() - 1);
+32        map.remove(val);
+33
+34        return true;
+35    }
+36
+37    public int getRandom() {
+38        return list.get(random.nextInt(list.size()));
+39    }
+40}

7f8f79e947ce4da006edd6e7ba4050c689828b37
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 10:21:48 +0530

    File updated at 8/22/2026, 10:21:48 AM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,25 +1,19 @@
-// Last updated: 8/22/2026, 10:18:06 AM
-1import java.util.*;
-2
-3class Solution {
-4    public int lengthOfLongestSubstring(String s) {
-5        HashSet<Character> set = new HashSet<>();
-6
-7        int left = 0;
-8        int ans = 0;
-9
-10        for (int right = 0; right < s.length(); right++) {
+// Last updated: 8/22/2026, 10:21:48 AM
+1class Solution {
+2    public boolean hasAlternatingBits(int n) {
+3        int prev = n & 1;
+4        n >>= 1;
+5
+6        while (n > 0) {
+7            int current = n & 1;
+8
+9            if (current == prev)
+10                return false;
 11
-12            while (set.contains(s.charAt(right))) {
-13                set.remove(s.charAt(left));
-14                left++;
-15            }
-16
-17            set.add(s.charAt(right));
-18
-19            ans = Math.max(ans, right - left + 1);
-20        }
-21
-22        return ans;
-23    }
-24}
+12            prev = current;
+13            n >>= 1;
+14        }
+15
+16        return true;
+17    }
+18}

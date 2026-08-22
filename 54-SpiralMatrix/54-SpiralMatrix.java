f440f5be229458b8059db3b45e7cb5fd9b4f2ae3
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 11:08:42 +0530

    File updated at 8/22/2026, 11:08:42 AM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,22 +1,32 @@
-// Last updated: 8/22/2026, 11:07:42 AM
-1class Solution {
-2    public int findDuplicate(int[] nums) {
-3
-4        int slow = nums[0];
-5        int fast = nums[0];
-6
-7        do {
-8            slow = nums[slow];
-9            fast = nums[nums[fast]];
-10        } while (slow != fast);
-11
-12        slow = nums[0];
-13
-14        while (slow != fast) {
-15            slow = nums[slow];
-16            fast = nums[fast];
-17        }
-18
-19        return slow;
-20    }
-21}
+// Last updated: 8/22/2026, 11:08:42 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int longestConsecutive(int[] nums) {
+5
+6        HashSet<Integer> set = new HashSet<>();
+7
+8        for (int n : nums)
+9            set.add(n);
+10
+11        int ans = 0;
+12
+13        for (int n : set) {
+14
+15            if (!set.contains(n - 1)) {
+16
+17                int current = n;
+18                int length = 1;
+19
+20                while (set.contains(current + 1)) {
+21                    current++;
+22                    length++;
+23                }
+24
+25                ans = Math.max(ans, length);
+26            }
+27        }
+28
+29        return ans;
+30    }
+31}

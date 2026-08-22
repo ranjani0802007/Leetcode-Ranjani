8cf03e6ee423db16b44920035b0e07bd96deff3f
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 10:18:07 +0530

    File updated at 8/22/2026, 10:18:06 AM

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,41 +1,25 @@
-// Last updated: 8/22/2026, 10:16:52 AM
+// Last updated: 8/22/2026, 10:18:06 AM
 1import java.util.*;
 2
-3class RandomizedSet {
-4
-5    ArrayList<Integer> list = new ArrayList<>();
-6    HashMap<Integer, Integer> map = new HashMap<>();
-7    Random random = new Random();
-8
-9    public RandomizedSet() {}
-10
-11    public boolean insert(int val) {
-12        if (map.containsKey(val))
-13            return false;
-14
-15        map.put(val, list.size());
-16        list.add(val);
-17
-18        return true;
-19    }
-20
-21    public boolean remove(int val) {
-22        if (!map.containsKey(val))
-23            return false;
-24
-25        int index = map.get(val);
-26        int last = list.get(list.size() - 1);
-27
-28        list.set(index, last);
-29        map.put(last, index);
-30
-31        list.remove(list.size() - 1);
-32        map.remove(val);
-33
-34        return true;
-35    }
-36
-37    public int getRandom() {
-38        return list.get(random.nextInt(list.size()));
-39    }
-40}
+3class Solution {
+4    public int lengthOfLongestSubstring(String s) {
+5        HashSet<Character> set = new HashSet<>();
+6
+7        int left = 0;
+8        int ans = 0;
+9
+10        for (int right = 0; right < s.length(); right++) {
+11
+12            while (set.contains(s.charAt(right))) {
+13                set.remove(s.charAt(left));
+14                left++;
+15            }
+16
+17            set.add(s.charAt(right));
+18
+19            ans = Math.max(ans, right - left + 1);
+20        }
+21
+22        return ans;
+23    }
+24}

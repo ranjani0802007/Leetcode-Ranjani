a0e84aa7d74d9282298b6ba8baa65e2c6ce2a73c
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-22 11:07:42 +0530

    File updated at 8/22/2026, 11:07:42 AM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,40 +1,22 @@
-// Last updated: 8/22/2026, 11:06:53 AM
+// Last updated: 8/22/2026, 11:07:42 AM
 1class Solution {
-2    public int[][] generateMatrix(int n) {
+2    public int findDuplicate(int[] nums) {
 3
-4        int[][] matrix = new int[n][n];
-5
-6        int top = 0, bottom = n - 1;
-7        int left = 0, right = n - 1;
-8        int value = 1;
-9
-10        while (top <= bottom) {
+4        int slow = nums[0];
+5        int fast = nums[0];
+6
+7        do {
+8            slow = nums[slow];
+9            fast = nums[nums[fast]];
+10        } while (slow != fast);
 11
-12            for (int i = left; i <= right; i++)
-13                matrix[top][i] = value++;
-14
-15            top++;
-16
-17            for (int i = top; i <= bottom; i++)
-18                matrix[i][right] = value++;
-19
-20            right--;
-21
-22            if (top <= bottom) {
-23                for (int i = right; i >= left; i--)
-24                    matrix[bottom][i] = value++;
-25
-26                bottom--;
-27            }
-28
-29            if (left <= right) {
-30                for (int i = bottom; i >= top; i--)
-31                    matrix[i][left] = value++;
-32
-33                left++;
-34            }
-35        }
-36
-37        return matrix;
-38    }
-39}
+12        slow = nums[0];
+13
+14        while (slow != fast) {
+15            slow = nums[slow];
+16            fast = nums[fast];
+17        }
+18
+19        return slow;
+20    }
+21}

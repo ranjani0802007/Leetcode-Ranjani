016e3395cb43c5fb56810f4c588e067833c840a2
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-28 09:47:30 +0530

    File updated at 8/28/2026, 9:47:24 AM

--- 901-OnlineStockSpan/901-OnlineStockSpan.java
+++ 901-OnlineStockSpan/901-OnlineStockSpan.java
@@ -1,51 +1,26 @@
-// Last updated: 8/28/2026, 9:45:13 AM
+// Last updated: 8/28/2026, 9:47:24 AM
 1class Solution {
-2    public int[] searchRange(int[] nums, int target) {
-3        int first = findFirst(nums, target);
-4        int last = findLast(nums, target);
+2    public boolean searchMatrix(int[][] matrix, int target) {
+3        int rows = matrix.length;
+4        int cols = matrix[0].length;
 5
-6        return new int[]{first, last};
-7    }
+6        int low = 0;
+7        int high = rows * cols - 1;
 8
-9    private int findFirst(int[] nums, int target) {
-10        int low = 0;
-11        int high = nums.length - 1;
-12        int ans = -1;
+9        while (low <= high) {
+10            int mid = low + (high - low) / 2;
+11
+12            int value = matrix[mid / cols][mid % cols];
 13
-14        while (low <= high) {
-15            int mid = low + (high - low) / 2;
-16
-17            if (nums[mid] == target) {
-18                ans = mid;
+14            if (value == target) {
+15                return true;
+16            } else if (value < target) {
+17                low = mid + 1;
+18            } else {
 19                high = mid - 1;
-20            } else if (nums[mid] < target) {
-21                low = mid + 1;
-22            } else {
-23                high = mid - 1;
-24            }
-25        }
-26
-27        return ans;
-28    }
-29
-30    private int findLast(int[] nums, int target) {
-31        int low = 0;
-32        int high = nums.length - 1;
-33        int ans = -1;
-34
-35        while (low <= high) {
-36            int mid = low + (high - low) / 2;
-37
-38            if (nums[mid] == target) {
-39                ans = mid;
-40                low = mid + 1;
-41            } else if (nums[mid] < target) {
-42                low = mid + 1;
-43            } else {
-44                high = mid - 1;
-45            }
-46        }
-47
-48        return ans;
-49    }
-50}
+20            }
+21        }
+22
+23        return false;
+24    }
+25}

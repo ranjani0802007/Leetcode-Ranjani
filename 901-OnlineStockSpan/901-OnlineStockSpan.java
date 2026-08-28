3fe58fd1a328cf212fadf2c0903f8719ebd70488
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-28 09:36:30 +0530

    File updated at 8/28/2026, 9:36:25 AM

--- 901-OnlineStockSpan/901-OnlineStockSpan.java
+++ 901-OnlineStockSpan/901-OnlineStockSpan.java
@@ -1,34 +1,84 @@
-// Last updated: 8/28/2026, 9:32:07 AM
-1class Solution {
-2    public int search(int[] nums, int target) {
-3        int low = 0;
-4        int high = nums.length - 1;
+// Last updated: 8/28/2026, 9:36:25 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int countCompleteSubstrings(String word, int k) {
 5
-6        while (low <= high) {
-7            int mid = low + (high - low) / 2;
+6        int n = word.length();
+7        int answer = 0;
 8
-9            if (nums[mid] == target) {
-10                return mid;
-11            }
+9        for (int i = 0; i < n; ) {
+10
+11            int j = i;
 12
-13            // Left half is sorted
-14            if (nums[low] <= nums[mid]) {
-15                if (nums[low] <= target && target < nums[mid]) {
-16                    high = mid - 1;
-17                } else {
-18                    low = mid + 1;
-19                }
-20            }
-21            // Right half is sorted
-22            else {
-23                if (nums[mid] < target && target <= nums[high]) {
-24                    low = mid + 1;
-25                } else {
-26                    high = mid - 1;
-27                }
-28            }
-29        }
+13            // Split whenever adjacent characters differ by more than 2
+14            while (j + 1 < n &&
+15                   Math.abs(word.charAt(j) - word.charAt(j + 1)) <= 2) {
+16                j++;
+17            }
+18
+19            answer += countSegment(
+20                    word.substring(i, j + 1), k
+21            );
+22
+23            i = j + 1;
+24        }
+25
+26        return answer;
+27    }
+28
+29    private int countSegment(String s, int k) {
 30
-31        return -1;
-32    }
-33}
+31        int n = s.length();
+32        int answer = 0;
+33
+34        // Number of distinct characters can be from 1 to 26
+35        for (int distinct = 1; distinct <= 26; distinct++) {
+36
+37            int length = distinct * k;
+38
+39            if (length > n) {
+40                break;
+41            }
+42
+43            int[] freq = new int[26];
+44
+45            for (int i = 0; i < length; i++) {
+46                freq[s.charAt(i) - 'a']++;
+47            }
+48
+49            if (valid(freq, distinct, k)) {
+50                answer++;
+51            }
+52
+53            for (int right = length; right < n; right++) {
+54
+55                freq[s.charAt(right) - 'a']++;
+56
+57                freq[s.charAt(right - length) - 'a']--;
+58
+59                if (valid(freq, distinct, k)) {
+60                    answer++;
+61                }
+62            }
+63        }
+64
+65        return answer;
+66    }
+67
+68    private boolean valid(int[] freq, int distinct, int k) {
+69
+70        int count = 0;
+71
+72        for (int f : freq) {
+73            if (f > 0) {
+74                if (f != k) {
+75                    return false;
+76                }
+77                count++;
+78            }
+79        }
+80
+81        return count == distinct;
+82    }
+83}

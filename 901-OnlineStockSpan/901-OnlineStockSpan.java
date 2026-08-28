009d398748ae245f5045323da2d3e08475b2f6b4
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-28 09:27:33 +0530

    File updated at 8/28/2026, 9:27:28 AM

--- 901-OnlineStockSpan/901-OnlineStockSpan.java
+++ 901-OnlineStockSpan/901-OnlineStockSpan.java
@@ -1,46 +1,39 @@
-// Last updated: 8/28/2026, 9:26:04 AM
+// Last updated: 8/28/2026, 9:27:28 AM
 1import java.util.*;
 2
 3class Solution {
-4    public int calculate(String s) {
-5        Stack<Integer> stack = new Stack<>();
-6
-7        int num = 0;
-8        char sign = '+';
-9
-10        for (int i = 0; i < s.length(); i++) {
-11            char c = s.charAt(i);
+4    public int subarraysWithKDistinct(int[] nums, int k) {
+5        return atMost(nums, k) - atMost(nums, k - 1);
+6    }
+7
+8    private int atMost(int[] nums, int k) {
+9        if (k == 0) {
+10            return 0;
+11        }
 12
-13            if (Character.isDigit(c)) {
-14                num = num * 10 + (c - '0');
-15            }
-16
-17            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
-18
-19                if (sign == '+') {
-20                    stack.push(num);
-21                } 
-22                else if (sign == '-') {
-23                    stack.push(-num);
-24                } 
-25                else if (sign == '*') {
-26                    stack.push(stack.pop() * num);
-27                } 
-28                else if (sign == '/') {
-29                    stack.push(stack.pop() / num);
-30                }
-31
-32                sign = c;
-33                num = 0;
-34            }
-35        }
-36
-37        int result = 0;
-38
-39        for (int x : stack) {
-40            result += x;
-41        }
-42
-43        return result;
-44    }
-45}
+13        Map<Integer, Integer> map = new HashMap<>();
+14
+15        int left = 0;
+16        int result = 0;
+17
+18        for (int right = 0; right < nums.length; right++) {
+19
+20            map.put(nums[right],
+21                    map.getOrDefault(nums[right], 0) + 1);
+22
+23            while (map.size() > k) {
+24                map.put(nums[left], map.get(nums[left]) - 1);
+25
+26                if (map.get(nums[left]) == 0) {
+27                    map.remove(nums[left]);
+28                }
+29
+30                left++;
+31            }
+32
+33            result += right - left + 1;
+34        }
+35
+36        return result;
+37    }
+38}

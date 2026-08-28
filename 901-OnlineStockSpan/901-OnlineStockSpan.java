ad77545595012b1857ce67d7fbac55adeaa0b9ca
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-08-28 09:24:25 +0530

    File updated at 8/28/2026, 9:24:20 AM

--- 901-OnlineStockSpan/901-OnlineStockSpan.java
+++ 901-OnlineStockSpan/901-OnlineStockSpan.java
@@ -1,23 +1,23 @@
-// Last updated: 8/28/2026, 9:22:32 AM
+// Last updated: 8/28/2026, 9:24:20 AM
 1import java.util.*;
 2
-3class StockSpanner {
-4
-5    Stack<int[]> stack;
+3class Solution {
+4    public boolean validateStackSequences(int[] pushed, int[] popped) {
+5        Stack<Integer> stack = new Stack<>();
 6
-7    public StockSpanner() {
-8        stack = new Stack<>();
-9    }
-10
-11    public int next(int price) {
-12        int span = 1;
-13
-14        while (!stack.isEmpty() && stack.peek()[0] <= price) {
-15            span += stack.pop()[1];
-16        }
-17
-18        stack.push(new int[]{price, span});
+7        int j = 0;
+8
+9        for (int x : pushed) {
+10            stack.push(x);
+11
+12            while (!stack.isEmpty() && 
+13                   j < popped.length && 
+14                   stack.peek() == popped[j]) {
+15                stack.pop();
+16                j++;
+17            }
+18        }
 19
-20        return span;
+20        return stack.isEmpty();
 21    }
 22}

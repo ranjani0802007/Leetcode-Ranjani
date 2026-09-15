e3a25e886c12f87ad21e1a3a12977fcf65b6496f
ranjani0802007 <ranjanisathishkumar802@gmail.com>
2026-09-15 16:28:47 +0530

    Create 2460-ApplyOperationsToAnArray

---
+++ 2460-ApplyOperationsToAnArray/2460-ApplyOperationsToAnArray.java
@@ -0,0 +1,27 @@
+// Last updated: 9/15/2026, 4:28:44 PM
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Apply operations sequentially
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        // Step 2: Shift all zeros to the end
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                result[index++] = nums[i];
+            }
+        }
+        // Remaining positions in result are already 0 by default
+
+        return result;
+    }
+}
